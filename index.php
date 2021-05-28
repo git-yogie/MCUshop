36839919b5a6cd5ea88fc5d13a771c8a9b66336b
git-yogie <yogie.prayoga35@gmail.com>
2021-05-28 19:45:32 +0700

    ok

--- index.php
+++ index.php
@@ -18,7 +18,6 @@ $slider = getAllData('slider');
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
     <link rel="stylesheet" href="http://localhost/MCUshop/css/main.css">
-    <!-- <link rel="stylesheet" href="/css/page1.css"> -->
     <title>Toko MCU</title>
 </head>
 
