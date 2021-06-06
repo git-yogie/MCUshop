42693ac3fe1ae4aa152f9feeb0e0cce70ab172ab
git-yogie <yogie.prayoga35@gmail.com>
2021-06-06 08:37:14 +0700

    menghilangkan pesanan di dashboard

--- admin/index.php
+++ admin/index.php
@@ -1,5 +1,5 @@
-<?php 
-include "template/header.php" ;
+<?php
+include "template/header.php";
 include 'dataSource\queryTable.php';
 ?>
 <div class="container">
@@ -20,68 +20,41 @@ include 'dataSource\queryTable.php';
             <div class="icon primary"><i class="fas fa-users"></i></div>
             <div class="body">
                 <p>Jumlah customer</p>
-                <span><?=num_rows('customer')?></span>
+                <span><?= num_rows('customer') ?></span>
             </div>
         </div>
         <div class="card-stat">
             <div class="icon danger"><i class="fas fa-box"></i></div>
             <div class="body">
                 <p>Jumlah Produk</p>
-                <span><?=num_rows('product')?></span>
+                <span><?= num_rows('product') ?></span>
             </div>
         </div>
         <div class="card-stat">
             <div class="icon warning"><i class="fas fa-clipboard-list"></i></div>
             <div class="body">
                 <p>Pesanan</p>
-                <span><?=num_rows('pesanan')?></span>
+                <span><?= num_rows('pesanan') ?></span>
             </div>
         </div>
         <div class="card-stat">
             <div class="icon success"><i class="far fa-handshake"></i></div>
             <div class="body">
                 <p>Produk terjual</p>
-                <span><?=num_rows_column('pesanan','status','3')?></span>
+                <span><?= num_rows_column('pesanan', 'status', '3') ?></span>
             </div>
         </div>
     </div>
     <br>
     <div class="dashboard-menu">
-    <div class="header"><h4>dashboard Menu</h4></div>
-       <div class="menu">
-       <button class="btn primary" onclick="window.location = 'slider.php'"><i class="far fa-image"></i> Slider</button>
-        <button class="btn primary" onclick="window.location = 'diskon.php'"><i class="fas fa-tags"></i>Diskon</button>
-        <button class="btn primary" onclick="window.location = 'pesan.php'"><i class="fas fa-envelope-open-text"></i> Pesan</button>
-       </div>
+        <div class="header">
+            <h4>dashboard Menu</h4>
+        </div>
+        <div class="menu">
+            <button class="btn primary" onclick="window.location = 'slider.php'"><i class="far fa-image"></i> Slider</button>
+            <button class="btn primary" onclick="window.location = 'diskon.php'"><i class="fas fa-tags"></i>Diskon</button>
+            <button class="btn primary" onclick="window.location = 'pesan.php'"><i class="fas fa-envelope-open-text"></i> Pesan</button>
+        </div>
     </div>
-    <div class="table-Admin">
-        <h3>Pesanan</h3>
-        <table>
-            <tr>
-                <td>No</td>
-                <td>Produk</td>
-                <td>Tanggal Pesan</td>
-                <td>Pemesan</td>
-                <td>Alamat</td>
-                <td>status</td>
-                <td>aksi</td>
-            </tr>
-            <tr>
-                <td>1</td>
-                <td>Asus G1 strix x</td>
-                <td>20-07-2021</td>
-                <td>Yogie Prayoga</td>
-                <td>Alamat</td>
-                <td>Menunggu Pembayaran</td>
-                <td>
-                    <button class="btn success"><i class="fas fa-check"></i></button>
-                    <button class="btn danger"><i class="fas fa-times"></i></button>
-                </td>
-            </tr>
-
-
-        </table>
-    </div>
-
 </div>
 <?php include "template/footer.php" ?>
